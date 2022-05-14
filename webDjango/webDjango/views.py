6742ba1269d35a661c8ec481215123fa18c74328
Juan Ignacio <zalbak87@gmail.com>
2022-05-14 18:23:16 -0300

    guardado del idex

--- webDjango/webDjango/views.py
+++ webDjango/webDjango/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 
 def saludo(request):
-    return HttpResponse('Hola te saludo desde mi pagina de django')
+    return render(request,'index.html',{})#reder recibe tres argumentos, primeero el request, segundo el html que renderizamos y el tercero un diccionario
